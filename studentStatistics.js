1b287120226d029c0a0d667d637a6d4f5fe8df70
Cuongne2406 <cainnguyen2005@gmail.com>
2025-12-15 10:51:05 +0700

    bai2

---
+++ studentStatistics.js
@@ -0,0 +1,16 @@
+const class1 = [
+    {
+        mssv: '123000991',
+        name: 'Nguyễn Trung Cường',
+        avgPoint: '9',
+        avgTrainingPoint: '9',
+        status: 'pass',
+    },
+    {
+        mssv: '123000111',
+        name: 'Nguyễn Hậu',
+        avgPoint: '9',
+        avgTrainingPoint: '9',
+        status: 'pass',
+    },
+];
